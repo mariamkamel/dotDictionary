0bfb7c34dad767abf574d404681f71bee212d700
Rawan Ashraf <Rawanelashry@gmail.com>
2017-12-08 00:52:53 +0200

    adding all windows in one css/html

--- main.js
+++ main.js
@@ -4,5 +4,5 @@ const {app, BrowserWindow} = electron;
 
 app.on('ready', () => {
    const mainWindow = new BrowserWindow({});  
-   mainWindow.loadURL(`file://${__dirname}/index.html`);
+   mainWindow.loadURL(`file://${__dirname}/src/index.html`);
 }); 

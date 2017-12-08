54447f28e427dbb785bc56cca4e673fae7720255
Khaled Mohamed <khal3d.mohamed@gmail.com>
2017-12-08 17:20:05 +0200

    Various bug fixes

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const electron = require('electron');
+const Trie = require('./src/DataStructures/Trie.js');
 
 const {app, BrowserWindow} = electron;
 
 app.on('ready', () => {
-   const mainWindow = new BrowserWindow({});  
+   const mainWindow = new BrowserWindow({
+       minWidth: 800,
+       minHeight:600
+   });  
    mainWindow.loadURL(`file://${__dirname}/src/index.html`);
 }); 
--- src/js/functions.js
+++ src/js/functions.js
@@ -1,120 +1,106 @@
-function openAddModal(){
-    var modal = document.getElementById("add");
-    var editmodal = document.getElementById("editmodal");
-    modal.style.minHeight="260pt";
-    modal.style.minWidth="350pt";
-    modal.style.display= "block";
-    editmodal.style.display="none";
-    
-}
-function addWord(){
-  var w =  document.getElementById('nameinput').value;
-  var t = document.getElementById('typeinput').value;
-  var d =document.getElementById('difinput').value;
-  var s = " ";
-  if (w.length==0){
-      s+="You Should Enter A Word.\n";
-  }
-   if(t.length==0){
-     s+="You Should Enter A Type.\n";
-  }
-   if (d.length==0){
-     s+="You Should Enter A Definition.";
-    
-  }
-
-  else {
-    appending(w,d,t);
-    document.getElementById("add").style.display ="none";
-   document.getElementById("division");
-   return;
-  }
-  alert(s);
-}
-function appending(word,definition,Type) {
-
-    var main = document.getElementById("division");
-    var req = document.getElementById("required");
-    var inside =req.childNodes[1];
-    var defDiv = req.childNodes[3];
-    var cloned = req.cloneNode(true);
-    var type = document.createElement("P");
-    var t = document.createTextNode(".");
-    type.appendChild(t);
-    t =document.createTextNode(Type);
-    type.appendChild(t);
-    t = document.createTextNode(".");
-    type.appendChild(t);
-    type.className += "typeParagraph";
-    var w = document.createElement("P");
-    var Word = document.createTextNode(word);
-    w.appendChild(Word);
-    w.className += "word";
-    var d = document.createElement("P");
-    var define = document.createTextNode(definition);
-    d.appendChild(define);
-    d.className += "definitionClass"; 
-    inside.replaceChild(w,inside.childNodes[1]);
-    inside.replaceChild(type,inside.childNodes[2]);
-    defDiv.replaceChild(d,defDiv.childNodes[1]);
-    main.appendChild(cloned);
-    req.style.display ="block";        
-    
+function openAddModal() {
+    const modal = document.getElementById("add");
+    const editmodal = document.getElementById("editmodal");
+    modal.style.minHeight = "260pt";
+    modal.style.minWidth = "350pt";
+    modal.style.display = "block";
+    editmodal.style.display = "none";
 }
 
-function deleteWord(){
-    alert("Are you sure you want to delete it? :(");
-}
-function editWord(){
-    var w =  document.getElementById('wordlbl').textContent;    
-    var t = document.getElementById('editTypeInput').value;
-    var d =document.getElementById('editDifInput').value;
-    var s = " ";
-     if(t.length==0){
-       s+="You Should Enter A Type.\n";
+function addWord() {
+    const word =  document.getElementById('nameinput').value;
+    const type = document.getElementById('typeinput').value;
+    const definition = document.getElementById('difinput').value;
+    let err = "";
+    if (!word.length) {
+        err = "You Should Enter A Word.";
+    } else if (!type.length) {
+        err = "You Should Enter A Type.";
+    } else if (!definition.length) {
+        err = "You Should Enter A Definition.";  
+    } else {
+        appendWord(word, definition, type);
+        document.getElementById("add").style.display = "none";
+        return;
     }
-     if (d.length==0){
-       s+="You Should Enter A Definition.";
-      
+    alert(err);
+}
+
+function appendWord(word, definition, type) {
+    let main = document.getElementById('division');
+    let element = document.getElementById('required').cloneNode(true);
+
+    let wordElement = element.childNodes[1].childNodes[1];
+    let typeElement = element.childNodes[1].childNodes[3];
+    let defElement = element.childNodes[3];
+    
+    element.childNodes[5].addEventListener('click', function(e) {
+        openEditModal(word, type, definition);
+    });
+
+    element.childNodes[7].addEventListener('click', function(e) {
+        console.log(word);
+        deleteWord();
+    });
+
+    wordElement.innerHTML = word;
+    typeElement.innerHTML = `.${type}.`;
+    defElement.innerHTML = definition;
+
+    element.style.display = 'block';
+    main.appendChild(element);
+}
+
+function deleteWord() {
+    console.log(
+    confirm("Are you sure you want to delete it? :("));
+}
+
+function editWord() {
+    const word =  document.getElementById('wordlbl').textContent;    
+    const type = document.getElementById('editTypeInput').value;
+    const definition = document.getElementById('editDifInput').value;
+    let err = "";
+    if (!type.length) {
+        err = "You Should Enter A Type.\n";
+    } else if (!definition.length) {
+        err = "You Should Enter A Definition."; 
+    } else {
+        appendWord(word, definition, type);
+        document.getElementById("editmodal").style.display = "none";
+        return;
     }
-    else {
-      appending(w,d,t);
-      document.getElementById("editmodal").style.display ="none";
-     document.getElementById("division");
-     return;
-    }
-    alert(s);
+    alert(err);
 }
-function openEditModal(){
-    var w =  document.getElementById('nameinput').value;
-    var modal = document.getElementById("editmodal");
-    var addmodal = document.getElementById("add");
+
+function openEditModal(word, type, definition) {
+    const modal = document.getElementById("editmodal");
+
+    document.getElementById('wordlbl').innerHTML = word;
+    document.getElementById('editTypeInput').value = type;
+    document.getElementById('editDifInput').value = definition;
     
-    document.getElementById('wordlbl').innerHTML = w;
-     
-    modal.style.minHeight="260pt";
-    modal.style.minWidth="350pt";
-    modal.style.display= "block";
-    addmodal.style.display="none";
-    
+    modal.style.minHeight = "260pt";
+    modal.style.minWidth = "350pt";
+    modal.style.display = "block";
 }
-function canceledit(){
-    document.getElementById("editmodal").style.display ="none";
-    document.getElementById("division");
-    
+
+function canceledit() {
+    document.getElementById("editmodal").style.display = "none";    
 }
-function canceladd(){
-    document.getElementById("add").style.display ="none";
-    document.getElementById("division");
-    
+
+function canceladd() {
+    document.getElementById("add").style.display = "none";
 }
+
 function speechRecognition() {
     alert('you have clicked');
-   }
-function submitSearch(){
-       alert('ANA MESH BUTTON :P')
-   }
-function showAllFunc(){
+}
+
+function submitSearch() {
+    alert('ANA MESH BUTTON :P')
+}
+
+function showAllFunc() {
     alert("Show All");
-    
-   }
+}

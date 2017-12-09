6287407ebc5353c8db65df83d339ddf909b709f8
Khaled Mohamed <khal3d.mohamed@gmail.com>
2017-12-09 17:49:10 +0200

    Integrating the Trie and Dictionary Database with the App

--- main.js
+++ main.js
@@ -1,12 +1,54 @@
-const electron = require('electron');
+const {app, BrowserWindow} = require('electron');
 const Trie = require('./src/DataStructures/Trie.js');
+const fs = require('fs');
 
-const {app, BrowserWindow} = electron;
+let dictionary = new Trie();
+let dictionaryDB;
+
+try {
+    dictionaryDB = require(`${__dirname}/dotDictionaryData.json`);
+} catch(err) {
+    dictionaryDB = {
+        data: [],
+    };
+    fs.writeFile(`${__dirname}/dotDictionaryData.json`, JSON.stringify(dictionaryDB), 'utf8', function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("The Dictionary Data File was created successfully.");
+        }
+    });
+}
+
+loadTrieData(dictionaryDB.data);
 
 app.on('ready', () => {
-   const mainWindow = new BrowserWindow({
-       minWidth: 800,
-       minHeight:600
-   });  
-   mainWindow.loadURL(`file://${__dirname}/src/index.html`);
-}); 
+    const mainWindow = new BrowserWindow({
+        minWidth: 800,
+        minHeight:600,
+        show: false
+    });
+    mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+    mainWindow.on('ready-to-show', () => {
+        mainWindow.show();
+    });
+});
+
+app.on('before-quit', () => {
+    dictionaryDB.data = dictionary.getAll();
+    fs.writeFile(`${__dirname}/dotDictionaryData.json`, JSON.stringify(dictionaryDB), 'utf8', function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("The Dictionary Data File was saved successfully.");
+        }
+    });
+})
+
+async function loadTrieData(dictionaryDB) {
+    for (let word of dictionaryDB) {
+        dictionary.insert(word.word, word.type, word.definition);
+    }
+}
+
+exports.dictionary = dictionary;
